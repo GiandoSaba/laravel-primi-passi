0e75126add9c5a0fa2c77eebdcb7c38fa30bef4b
GiandoSaba <giandosaba997@gmail.com>
2022-02-10 13:15:19 +0100

    dati in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        ul {
+            list-style: none;
+            display: flex;
+        }
+        li {
+            margin-right: 1em;
+        }
+    </style>
 </head>
 <body>
+    <header>
+        <ul>
+            @foreach ($headerLink as $link)
+                <li><a href="">{{ $link }}</a></li>
+            @endforeach
+        </ul>
+    </header>
     <h1>My Homepage</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = [
+        'headerLink' => [
+            'Home',
+            'Contatti',
+            'Info',
+            'Acquista Ora'
+        ]
+    ];
+    return view('home', $data);
 });
